44e14a7199dbdb862595f99157417b254671ce8c
guiliangdong <guiliangdong@sina.com>
2018-08-15 12:16:45 +0800

    647. Palindromic Substrings

---
+++ examples/src/main/java/leetcode/dynamicprogramming/medium/PalindromicSubstrings.java
@@ -0,0 +1,25 @@
+package leetcode.dynamicprogramming.medium;
+
+/**
+ * Given a string, your task is to count how many palindromic substrings in this
+ * string.
+ * 
+ * The substrings with different start indexes or end indexes are counted as
+ * different substrings even they consist of same characters.
+ * 
+ * @author guild
+ * @date 2018.8.15
+ */
+public class PalindromicSubstrings {
+
+	public static void main(String[] args) {
+		PalindromicSubstrings s = new PalindromicSubstrings();
+
+	}
+
+	public int countSubstrings(String s) {
+
+		return 0;
+	}
+
+}
