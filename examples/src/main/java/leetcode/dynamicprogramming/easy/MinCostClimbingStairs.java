c866a2ba1621b1712b775b3268c4e9fd7833d69d
guiliangdong <guiliangdong@sina.com>
2018-08-15 11:18:45 +0800

    746. Min Cost Climbing Stairs

---
+++ examples/src/main/java/leetcode/dynamicprogramming/easy/MinCostClimbingStairs.java
@@ -0,0 +1,36 @@
+package leetcode.dynamicprogramming.easy;
+
+import java.util.Arrays;
+
+/**
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0
+ * indexed).
+ * 
+ * Once you pay the cost, you can either climb one or two steps. You need to
+ * find minimum cost to reach the top of the floor, and you can either start
+ * from the step with index 0, or the step with index 1.
+ * 
+ * @author guild
+ * @date 2018.8.15
+ */
+public class MinCostClimbingStairs {
+	
+	public static void main(String[] args) {
+		MinCostClimbingStairs s = new MinCostClimbingStairs();
+		System.out.println(s.minCostClimbingStairs(new int[] { 10, 15, 20 }));
+		System.out.println(s.minCostClimbingStairs(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }));
+	}
+
+	public int minCostClimbingStairs(int[] cost) {
+		if (cost.length < 2) {
+			return 0;
+		}
+		
+		int[] res = Arrays.copyOf(cost, cost.length);
+		for (int i = 2; i < cost.length; i++) {
+			res[i] += Math.min(res[i - 2], res[i - 1]);
+		}
+		return Math.min(res[res.length - 1], res[res.length - 2]);
+	}
+
+}
