f2aa1eb50e4dd388d81423b5554822a5477cdb84
guild <guiliangdong@sina.com>
2018-08-21 18:42:24 +0800

    tree 1

--- examples/src/main/java/leetcode/tree/easy/SymmetricTree.java
+++ examples/src/main/java/leetcode/tree/easy/SymmetricTree.java
@@ -1,7 +1,5 @@
 package leetcode.tree.easy;
 
-import java.util.List;
-
 import leetcode.helper.TreeNode;
 
 public class SymmetricTree {
@@ -9,17 +7,30 @@ public class SymmetricTree {
 	public static void main(String[] args) {
 		SymmetricTree s = new SymmetricTree();
 
+		TreeNode node = new TreeNode(1);
+		node.left = new TreeNode(2);
+		node.right = new TreeNode(2);
+		node.left.left = new TreeNode(3);
+		node.left.right = new TreeNode(4);
+		node.right.left = new TreeNode(4);
+		node.right.right = new TreeNode(3);
+		System.out.println(s.isSymmetric(node));
 	}
 
 	public boolean isSymmetric(TreeNode root) {
-		if (root == null) {
-			return true;
-		}
-		return false;
+		return isSymmetric(root, root);
 	}
 	
-	private void isSymmetric(List<List<Integer>> res, List<TreeNode> nodes) {
-		
+	private boolean isSymmetric(TreeNode left, TreeNode right) {
+		if (left == null && right == null) {
+			return true;
+		}
+		if (left == null || right == null) {
+			return false;
+		}
+		return left.val == right.val
+				&& isSymmetric(left.left, right.right)
+				&& isSymmetric(left.right, right.left);
 	}
 
 }
